c480a96b43dd132b9ba568908f19ab7137c188bd
Stefan Koch <uplink.coder@googlemail.com>
2017-07-07 13:48:27 +0200

    intial add

---
+++ source/app.d
@@ -0,0 +1,195 @@
+import arsd.simpledisplay;
+import arsd.minigui;
+import std.conv;
+
+uint b4toi(ubyte[4] b4)
+{
+    uint result;
+
+    foreach (i, b; b4)
+    {
+        result |= b << i * 8;
+    }
+
+    return result;
+}
+
+struct DebuggerClientState
+{
+    ubyte[8192] recvBuffer;
+
+    uint dmdIp = b4toi([127, 0, 0, 1]); /// not use for now
+    ushort dmdPort; /// a non-zero means we are connect on that port;
+    string source;
+    uint currentLine;
+
+    string VarName;
+    ushort* NumberVariable;
+    uint Number;
+    bool numberInput;
+
+    void delegate(string) StatusLine = null;
+
+    void UpdateStatusLine()
+    {
+        StatusLine(VarName ? VarName ~ to!string(Number) : to!string(Number));
+    }
+
+    void NumberInput(ushort* varP, string varName = null)
+    {
+        if (varName)
+        {
+            VarName = varName ~ ": "; 
+            StatusLine(VarName);
+        }
+        numberInput = true;
+        NumberVariable = varP;
+    }
+}
+
+ushort findDmd(ushort[] portList)
+{
+    return 0;
+}
+
+void main()
+{
+
+    DebuggerClientState state;
+
+    auto window = new SimpleWindow(Size(1024, 786), "ctfe_dbg");
+    immutable textStart = window.height / 2;
+
+    int y = textStart;
+
+    void StatusLine(string text)
+    {
+        auto painter = window.draw();
+        int y = window.height - painter.fontHeight - 2;
+
+        if (text is null)
+        {
+            painter.outlineColor = Color.white;
+            painter.fillColor = Color.white;
+            painter.drawRectangle(Point(0, y), window.width, painter.fontHeight + 2);
+        }
+
+        painter.outlineColor = Color.red;
+        painter.fillColor = Color.gray;
+        painter.drawRectangle(Point(0, y), window.width, painter.fontHeight + 2);
+
+        painter.outlineColor = Color.green;
+        painter.drawText(Point(10, y), text);
+    }
+
+    state.StatusLine = &StatusLine;
+
+    void addLine(string text)
+    {
+        auto painter = window.draw();
+        //painter.fontHeight = window.height / 80;
+
+        if (y + painter.fontHeight * 3 >= window.height)
+        {
+            painter.scrollArea(Point(0, textStart), window.width,
+                    window.height - painter.fontHeight*4, 0, painter.fontHeight);
+            y -= painter.fontHeight + 2;
+        }
+
+        painter.outlineColor = Color.red;
+        painter.fillColor = Color.black;
+        painter.drawRectangle(Point(0, y), window.width, painter.fontHeight + 2);
+
+        painter.outlineColor = Color.green;
+
+        painter.drawText(Point(10, y), text);
+
+        y += painter.fontHeight + 2;
+    }
+
+    window.setResizeGranularity(2, 2);
+    //window2.eventLoop(1000, delegate () {return ;});
+    window.eventLoop(4000, delegate() {
+        with (state)
+        {
+            if (!dmdPort && !numberInput)
+            {
+                ushort portList[] = [0xc7fe];
+                dmdPort = findDmd(portList);
+                if (dmdPort)
+                {
+                    // TODO on succsessful connection
+                }
+                else
+                {
+                    StatusLine("Did not find no dmd on ports: " ~ to!string(portList));
+                }
+            }
+
+            //        addLine("Timer went off!");
+            return;
+        }
+    }, (KeyEvent event) {
+        with (state)
+        {
+            import std.datetime : Clock, msecs;
+
+            alias now = Clock.currTime;
+            static typeof(now()) lastTime;
+            static typeof(event.key) oldKey;
+
+            if (!event.pressed)
+            {
+                return;
+            }
+
+            lastTime = now;
+            oldKey = event.key;
+
+            if (event.key >= Key.N0 && event.key <= Key.N9)
+            {
+                import std.math;
+
+                if (numberInput)
+                {
+                    uint n = (event.key - Key.N0);
+
+                    Number *= 10;
+                    Number += n;
+                    UpdateStatusLine();
+                }
+            }
+            else
+            {
+                if (numberInput)
+                {
+                    if (event.key == Key.Backspace)
+                    {
+                        Number /= 10;
+                        UpdateStatusLine();
+                        return;
+                    }
+                    numberInput = false;
+                    *NumberVariable = cast(ushort) Number;
+                    addLine("Number: " ~ to!string(Number));
+                    Number = 0;
+                    StatusLine(null);
+                }
+                else
+                    addLine(to!string(event));
+
+                if (event.key == Key.F4 || event.key == Key.Escape || event.key == Key.Q)
+                    window.close();
+                if (event.key == Key.P)
+                {
+                    NumberInput(&dmdPort, "dmdPort");
+                }
+
+            }
+
+        }
+    }, delegate(MouseEvent event) {
+        return;
+        //  addLine(to!string(event));
+    },);
+}
